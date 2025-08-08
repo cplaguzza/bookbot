28ad75fb15ecd8e5fc4548bba5abcbfee572b9ab
cplaguzza <cplaguzza@gmail.com>
2025-08-08 11:16:02 -0400

    added sort function to word count. added output formatting

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, get_sorted_dict
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_characters = get_num_characters(text)
-    print(f"{num_words} words found in the document")
-    print(f"{num_characters}")
-
+    sorted_dict = get_sorted_dict(num_characters)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char_dict in sorted_dict:
+        if char_dict["char"].isalpha():
+            print(f"{char_dict['char']}: {char_dict['num']}")
+    print("============= END ===============")
+   
 def get_book_text(path):
     with open(path) as f:
         return f.read()
--- stats.py
+++ stats.py
@@ -12,5 +12,10 @@ def get_num_characters(text):
             num_characters[character] = 1
     return num_characters 
      
-
-
+def get_sorted_dict(num_characters):
+    dict_list = []
+    for character in num_characters:
+        count = num_characters[character]
+        dict_list.append({"char": character, "num": count})
+    dict_list.sort(reverse=True, key=lambda item: item["num"])
+    return dict_list
