808a88864e3522100f4321dc02dd13cbfdd676b5
cplaguzza <cplaguzza@gmail.com>
2025-08-09 18:02:39 -0400

    changed main.py to support book path input using sys.argv

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
+import sys
 from stats import get_num_words, get_num_characters, get_sorted_dict
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    num_characters = get_num_characters(text)
-    sorted_dict = get_sorted_dict(num_characters)
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for char_dict in sorted_dict:
-        if char_dict["char"].isalpha():
-            print(f"{char_dict['char']}: {char_dict['num']}")
-    print("============= END ===============")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1] 
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        num_characters = get_num_characters(text)
+        sorted_dict = get_sorted_dict(num_characters)
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for char_dict in sorted_dict:
+            if char_dict["char"].isalpha():
+                print(f"{char_dict['char']}: {char_dict['num']}")
+        print("============= END ===============")
    
 def get_book_text(path):
     with open(path) as f:
